8b1982696497fda8f5a230721498ec500c8824fc
le minh tuyen <leminhtuyen280906@gmail.com>
2025-07-12 14:06:30 +0700

    Buoi1-Java

--- src/buoi1.java
+++ src/buoi1.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class buoi1 {
     public static void main(String[] args) {
         System.out.printf("Hello world");
     }
